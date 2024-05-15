b61165645de3a9c9b466efa30ec8584484f918c2
tgsauz <tomgandulfo@gmail.com>
2024-05-15 03:38:56 -0300

    For deployment in netlify.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 export default {
     build: {
       rollupOptions: {
-        input: '/SPA-ABSTISA/main.js',
+        input: '/main.js',
         external: [
           '/router.js',
+          '/styles.css',
+          '/index.html',
+          '/components/nav.js',
+          '/components/colors.js',
+          '/components/home.js',
+          '/components/about.js',
+          '/components/notfound.js',
+          '/public/lock.svg',
+          '/public/unlock.svg',
+          '/public/TGicon.jpg',
         ],
       },
     },
